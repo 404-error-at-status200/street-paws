de78b20e50f83413835a750ca193773a550521ca
Steve <steve1818fernandes@gmail.com>
2023-04-09 08:52:47 +0530

    project-template

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo  from "./logo";
+import Profile from "./profile"
+import Hamburger from "./hamburger"
+
+
+function Main(){
+    return (
+        <>
+        </>
+    );
+} 
+export default Main
---
+++ src/preset.js
@@ -0,0 +1,61 @@
+const preset =  {
+    home : {
+        navbar : {
+            logo : {
+                link : "../../assets/sp-logo.jpg",
+                style : {
+                    height : "3.5em"
+                }
+            }
+        }
+    },
+}
+
+export default preset
+
+// {
+//     home: {
+//         navbar: {
+//             "logo":{
+//                 "style":""
+//             },
+//             "profile":{
+//                 "default-logo":"../assets/dog-profile.png",
+//                 "style":{"height":"10px"}
+//             },
+//             "hamburger-menu":{
+//                 "style":""
+//             }
+//         },
+//         "carousel":[
+//         {
+//             "picture":"../assets/poster1-pic.png",
+//             "heading":"Report a sick animal",
+//             "sub-heading":"",
+//             "description":"Saving an animal is just as simple as taking a picture and writing a short description about what you fell could be the cause and we will connect you with a nearby Vet who would assist you on what to do next",
+//             "button":"Save a Life"
+//         },
+//         {
+//             "picture":"../assets/poster2-pic.png",
+//             "heading":"Community Chat",
+//             "sub-heading":"",
+//             "description":"You can join our committee by just signing in and ask anyone for any advise regarding your pet or a stray animal and get answers from them in a jiffy ",
+//             "button":"Interact"
+//         },{
+//             "picture":"../assets/poster3-pic.png",
+//             "heading":"Talk with a Bot",
+//             "sub-heading":"",
+//             "description":"If you feel your situation might be uncommon or you just want to keep your privacy, you could chat with out AI-powered chatbot to get answers regarding any health issues regarding an animal",
+//             "button":"Chat"
+//         }],
+//         "footer":{
+//             "location-button":{
+//                 "image":"",
+//                 "default-text":"Nearby Vets"
+//             },
+//             "chatbot":{
+//                 "chatbot-logo":""
+//             }
+//         }
+//     }
+// }
